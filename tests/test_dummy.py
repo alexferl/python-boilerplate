3217c25ce7a4834c37d991be14e73993620f1fca
alexferl <me@alexferl.com>
2024-04-20 17:39:51 -0400

    add test
    Signed-off-by: alexferl <me@alexferl.com>

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,2 +1,10 @@
-def test_dummy():
-    pass
+import unittest
+
+
+class DummyTestCase(unittest.TestCase):
+    def test_dummy(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
